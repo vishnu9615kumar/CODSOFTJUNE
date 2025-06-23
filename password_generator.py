5fcc5cf6263c3cc1a51dcde5b9e17845b5d7f1f0
Vishnu Kumar <shiva143.kmrsingh@gmail.com>
2025-06-24 00:56:35 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,28 @@
 import random
 import string
 
-print("Welcome to Password Generator!")
-length = int(input("Enter the length of password: "))
+def generate_password():
+    while True:
+        try:
+            length = int(input("\nEnter the length of password (or 0 to exit): "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
 
-characters = string.ascii_letters + string.digits + string.punctuation
-password = ''.join(random.choice(characters) for _ in range(length))
+        if length == 0:
+            print("Exited from current session.")
+            break
 
-print("Generated Password is:", password)
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(length))
+        print("Generated Password is:", password)
+
+while True:
+    print("\nWelcome to Password Generator!")
+    generate_password()
+
+    again = input("\nDo you want to generate more passwords? (y/n): ").lower()
+    if again != 'y':
+        print("Goodbye! Thank you for using Password Generator.")
+        break
+
